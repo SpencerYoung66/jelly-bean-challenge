9df4adfa07c86974b8a512d9acb17059454c2711
Spencer Young <spenceryoung127@gmail.com>
2024-10-23 12:55:44 -0600

    Have functioning API calls (fake data)

--- Django/api/urls.py
+++ Django/api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('view', views.view),
+    path('add', views.add),
+    path('delete', views.delete),
+    path('update', views.update),
 ]
--- Django/api/views.py
+++ Django/api/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Flavor
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("This is the API home screen, please input a valid endpoint")
+
+def view(request):
+    flavors = Flavor.objects.all().values()
+    return JsonResponse(list(flavors), safe=False)
+
+def add(request):
+    try:
+        result = Flavor.objects.create(name="Test")
+        data = request.POST
+        return HttpResponse(data)
+        return JsonResponse({"status": "success", "flavor": {"id": result.id, "name": result.name}})
+    except Exception as e:
+        return JsonResponse({"status": "failed", "message": f"{e}"})
+
+def delete(request):
+    try:
+        result = Flavor.objects.filter(id=8).delete()
+        return JsonResponse({"status": "success", "numDeleted": result[0]})
+    except Exception as e:
+        return JsonResponse({"status": "failed", "message": f"{e}"})
+
+def update(request):
+    try:
+        result = Flavor.objects.filter(id=9).update(name="Test Again")
+        return JsonResponse({"status": "success", "numUpdated": result})
+    except Exception as e:
+        return JsonResponse({"status": "failed", "message": f"{e}"})
