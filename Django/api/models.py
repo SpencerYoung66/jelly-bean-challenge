c70df84f75cd6c21f8f6308edc2458a06b92d9ab
Spencer Young <spenceryoung127@gmail.com>
2024-10-23 11:41:52 -0600

    Set up Database and Added Flavor Table

--- Django/api/models.py
+++ Django/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Flavor(models.Model):
+    name = models.CharField(max_length=256, null=False)
+
+    def __str__(self):
+        return self.name
